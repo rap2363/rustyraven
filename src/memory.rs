a26f75704a52e2a45e6d6ddfa6ea55d4e947e39c
Rohan Paranjpe <rap2363@gmail.com>
2026-06-28 19:56:26 -0700

    memory

---
+++ src/memory.rs
@@ -0,0 +1,106 @@
+use std::ops::{Deref, DerefMut};
+
+const RAM_SIZE: u16 = 0x0800;
+
+// Represents a static, contiguous layout of memory (in bytes) and offers
+// low-level API's for reading and writing. Multiple segments are used
+// to build up main memory (RAM) for the CPU.
+struct MemorySegment<const N: usize> {
+    data: [u8; N],
+}
+
+impl<const N: usize> MemorySegment<N> {
+    // Initializes the segment to all zeros.
+    pub fn initialize() -> Self {
+        Self {
+            data: [0; N],
+        }
+    }
+
+    pub fn set_byte(&mut self, address: usize, value: u8) {
+        self.data[address] = value;
+    }
+
+    pub fn get_byte(&self, address: usize) -> u8 {
+        self.data[address]
+    }
+}
+
+struct CpuMemory {
+    ram: MemorySegment<0x0800>,
+    lower_io: MemorySegment<0x0008>,
+    upper_memory: MemorySegment<0xC000>,
+}
+
+impl CpuMemory {
+    fn initialize() -> Self {
+        Self {
+            ram: MemorySegment::<0x0800>::initialize(),
+            lower_io: MemorySegment::<0x0008>::initialize(),
+            upper_memory: MemorySegment::<0xC000>::initialize(),
+        }
+    }
+
+    pub fn set_byte(&mut self, address: u16, value: u8) {
+        if address < 0x2000 {
+            // RAM
+            let ram_address = address % 0x0800;
+            self.ram.set_byte(ram_address as usize, value);
+        } else if address < 0x4000 {
+            // Lower I/O
+            let lower_io_address = (address - 0x2000) % 0x0008;
+            println!("{}", lower_io_address);
+            self.lower_io.set_byte(lower_io_address as usize, value);
+        } else {
+            // Upper Memory
+            let upper_memory_address = address - 0x4000;
+            self.upper_memory.set_byte(upper_memory_address as usize, value);
+        }
+    }
+
+    pub fn get_byte(&self, address: u16) -> u8 {
+        if address < 0x2000 {
+            // RAM
+            let ram_address = address % 0x0800;
+            self.ram.get_byte(ram_address as usize)
+        } else if address < 0x4000 {
+            // Lower I/O
+            let lower_io_address = (address - 0x2000) % 0x0008;
+            self.lower_io.get_byte(lower_io_address as usize)
+        } else {
+            // Upper Memory
+            let upper_memory_address = address - 0x4000;
+            self.upper_memory.get_byte(upper_memory_address as usize)
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_16_byte_memory() {
+        let mut memory = MemorySegment::<16>::initialize();
+        assert_eq!(memory.get_byte(3), 0);
+        memory.set_byte(2, b'A');
+        assert_eq!(memory.get_byte(2), b'A');
+    }
+
+    #[test]
+    fn test_memory_mirroring() {
+        let mut cpu_memory = CpuMemory::initialize();
+        cpu_memory.set_byte(0x0803, 42);
+        cpu_memory.set_byte(0x2009, 43);
+        // Assert that the write can be read in a "mirrored" way throughout RAM.
+        assert_eq!(cpu_memory.get_byte(0x0003), 42);
+        assert_eq!(cpu_memory.get_byte(0x0803), 42);
+        assert_eq!(cpu_memory.get_byte(0x1003), 42);
+        assert_eq!(cpu_memory.get_byte(0x1803), 42);
+        // And lower I/O
+        assert_eq!(cpu_memory.get_byte(0x2001), 43);
+        assert_eq!(cpu_memory.get_byte(0x2009), 43);
+        assert_eq!(cpu_memory.get_byte(0x2011), 43);
+    }
+}
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,13 +1,129 @@
+#[derive(Debug, Default)]
+struct StatusFlags {
+    n: bool,
+    z: bool,
+    c: bool,
+    i: bool,
+    d: bool,
+    v: bool,
+}
+
+struct StatusFlagsBuilder {
+    inner: StatusFlags,
+}
+
+impl StatusFlagsBuilder {
+    fn set_negative(mut self) -> Self {
+        self.inner.n = true;
+        self
+    }
+
+    fn clear_negative(mut self) -> Self {
+        self.inner.n = false;
+        self
+    }
+
+    fn set_zero(mut self) -> Self {
+        self.inner.z = true;
+        self
+    }
+
+    fn clear_zero(mut self) -> Self {
+        self.inner.z = false;
+        self
+    }
+
+    fn set_carry(mut self) -> Self {
+        self.inner.c = true;
+        self
+    }
+
+    fn clear_carry(mut self) -> Self {
+        self.inner.c = false;
+        self
+    }
+
+    fn set_interrupt(mut self) -> Self {
+        self.inner.i = true;
+        self
+    }
+
+    fn clear_interrupt(mut self) -> Self {
+        self.inner.i = false;
+        self
+    }
+
+    fn set_overflow(mut self) -> Self {
+        self.inner.v = true;
+        self
+    }
+
+    fn clear_overflow(mut self) -> Self {
+        self.inner.v = false;
+        self
+    }
+
+    fn build(self) -> StatusFlags {
+        StatusFlags {
+            n: self.inner.n,
+            z: self.inner.z,
+            c: self.inner.c,
+            i: self.inner.i,
+            d: self.inner.d,
+            v: self.inner.v,
+        }
+    }
+}
+
+impl StatusFlags {
+    fn new() -> StatusFlagsBuilder {
+        StatusFlagsBuilder {
+            inner: StatusFlags::default()
+        }
+    }
+
+    fn from(status_flags: Self) -> StatusFlagsBuilder {
+        StatusFlagsBuilder {
+            inner: status_flags
+        }
+    }
+}
+
+#[derive(Debug)]
 enum Opcode {
     ADC,
 }
 
+#[derive(Debug)]
 struct OpcodeResult {
     num_cycles: usize,
-    
+    status: StatusFlags,
 }
-impl Opcode {
-    fn execute(&self) -> {
 
+impl Opcode {
+    fn execute(&self) -> OpcodeResult {
+        match self {
+            Opcode::ADC => Self::adc_execute(0, 0, false, StatusFlags::default()),
+        }
+    }
+
+    fn adc_execute(a: u8, m: u8, c: bool, status: StatusFlags) -> OpcodeResult {
+        OpcodeResult {
+            num_cycles: 0,
+            status: status,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_with_carry() {
+        let x = Opcode::ADC;
+        let result = x.execute();
+        println!("{:?}", result);
+        assert_eq!(1, 2);
     }
 }
